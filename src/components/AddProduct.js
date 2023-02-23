692e0926ddd2f5085c919692eba34e0b49ced9c7
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2023-02-23 17:58:15 +0530

    call api for add Product

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -5,8 +5,20 @@ const Addproduct = () => {
   const [price, setPrice] = React.useState("");
   const [category, setCategory] = React.useState("");
   const [company, setCompany] = React.useState("");
-  const addProduct = () => {
+  const addProduct = async () => {
     console.warn(name, price, category, company);
+    const userId = JSON.parse(localStorage.getItem("user"))._id;
+    let result = await fetch("http://localhost:5000/add-product", {
+      method: "post",
+      body: JSON.stringify({ name, price, category, company, userId }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
   };
   return (
     <div className="product">
