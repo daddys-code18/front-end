bcee65fcc26438a536a90a39c7898a0edf5828b1
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2023-03-01 00:52:02 +0530

    search ui

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -13,7 +13,7 @@ function ProductList() {
   };
   const deleteProduct = async (id) => {
     console.warn(id);
-    let result = await fetch(` http://localhost:5000/product/${id}`, {
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
       method: "Delete",
     });
     result = await result.json();
@@ -21,10 +21,28 @@ function ProductList() {
       getProducts();
     }
   };
+  const searchHandle = async (event) => {
+    let key = event.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
   console.warn("products", products);
   return (
     <div className="product-list">
       <h1>Product List</h1>
+      <input
+        type="text"
+        className="search-product-box"
+        placeholder="Search  Product"
+        onChange={searchHandle}
+      />
       <ul>
         <li>Sr:No</li>
         <li>Name</li>
@@ -33,18 +51,22 @@ function ProductList() {
         <li>Operation</li>
       </ul>
 
-      {products.map((item, index) => (
-        <ul key={item._id}>
-          <li>{index++}</li>
-          <li>{item.name}</li>
-          <li>${item.price}</li>
-          <li>{item.category ? item.category : "mobile"}</li>
-          <li>
-            <button onClick={() => deleteProduct(item._id)}>Delete</button>
-            <Link to={"/update/" + item._id}>Update</Link>
-          </li>
-        </ul>
-      ))}
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index++}</li>
+            <li>{item.name}</li>
+            <li>${item.price}</li>
+            <li>{item.category ? item.category : "mobile"}</li>
+            <li>
+              <button onClick={() => deleteProduct(item._id)}>Delete</button>
+              <Link to={"/update/" + item._id}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No Result Found</h1>
+      )}
     </div>
   );
 }
